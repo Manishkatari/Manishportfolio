6e3dc5495eee3e92b938d55d948548a0a076aa3b
Katari Manish <katarimanish123@>
2026-03-27 14:47:01 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // server:{
-  //   port:5173,
-  //   proxy:{
-  //     '/api':{
-  //       target:'https://portfolioserver-7v17.onrender.com',
-  //       changeOrigin:true,
-  //     }
-  //   }
-  // }
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // The third parameter '' allows loading variables without the VITE_ prefix if needed.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: "/Manishportfolio/",
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          // Access the variable from the loaded env object
+          target: env.VITE_API_TARGET, 
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    }
+  }
 })
